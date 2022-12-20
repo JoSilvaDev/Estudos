dab2c567fcc2c7cc8d54748eb2671a99f90e075b
JoSilvaDev <joilsons175@gmail.com>
2022-12-20 17:23:51 -0300

    up

---
+++ OneBitCode/aulas gratuitas/scrtipt.js
@@ -0,0 +1,28 @@
+function calculateTip(event){
+    event.preventDefault();
+    let bill = document.getElementById('bill').value;
+    let serviceQuality = document.getElementById('serviceQuality').value;
+    let numOfPeople = document.getElementById('people').value;
+
+    if ( bill == "" | serviceQuality == 0){
+        alert("Por favor, preencha os valores")
+        return;
+    }
+
+    if (numOfPeople == "" | numOfPeople <= 1){
+        numOfPeople = 1;
+        document.getElementById('each').style.display = "none"
+    } else{
+        document.getElementById('each').style.display = "block"
+    }
+
+    let total = (bill * serviceQuality) / numOfPeople;
+    total = total.toFixed(2);
+    document.getElementById('tip').innerHTML = total;
+    document.getElementById('totalTip').style.display = 'block'
+}
+
+document.getElementById('totalTip').style.display = "none"
+document.getElementById('each').style.display = "none"
+
+document.getElementById('tipsForm').addEventListener('submit', calculateTip);
